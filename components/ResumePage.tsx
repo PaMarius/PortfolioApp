709334e60136e4bf28fd092bc5b3748b3e153de0
PaMarius <marius_maryus1997@yahoo.com>
2023-11-29 02:24:14 +0200

    resolve ResumePage error

--- components/ResumePage.tsx
+++ components/ResumePage.tsx
@@ -3,7 +3,6 @@ import { NewHeader } from "./NewHeader";
 export const ResumePage = () => {
   return (
     <div className="flex flex-col h-full w-full justify-center items-center">
-      <NewHeader />
       <div className="w-full h-bodyContent"></div>
     </div>
   );
